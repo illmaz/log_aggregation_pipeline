2dfd66d98d44d737dffed0a29482eac3e4a1886e
Efe Yilmaz <info.eyilmaz@gmail.com>
2026-05-19 22:14:48 +0300

    add log line parser with struct-based field extraction

--- watcher.c
+++ watcher.c
@@ -1,6 +1,28 @@
 #include <stdio.h>
 #include <fcntl.h>
 #include <unistd.h>
+#include <string.h>
+
+typedef struct {
+    char timestamp[32];
+    char level[10];
+    char message[256];
+} LogEntry;
+
+void parse_line(char *line) {
+    LogEntry entry;
+    char date[16];
+    char time[16];
+
+    int parsed = sscanf(line, "%s %s %s %[^\n]",
+                        date, time, entry.level, entry.message);
+
+    if (parsed == 4) {
+        snprintf(entry.timestamp, sizeof(entry.timestamp), "%s %s", date, time);
+        printf("TIMESTAMP: %s | LEVEL: %s | MESSAGE: %s\n",
+               entry.timestamp, entry.level, entry.message);
+    }
+}
 
 int main() {
     int fd = open("test.log", O_RDONLY);
@@ -10,19 +32,24 @@ int main() {
     }
 
     lseek(fd, 0, SEEK_END);
-
     printf("Watching test.log...\n");
 
     char buffer[4096];
 
-     while (1) {
-    int bytes_read = read(fd, buffer, sizeof(buffer));
+    while (1) {
+        int bytes_read = read(fd, buffer, sizeof(buffer));
 
-    if (bytes_read > 0) {
-        write(1, buffer, bytes_read);
-    } else {
-        sleep(1);
+        if (bytes_read > 0) {
+            buffer[bytes_read] = '\0';
+            char *line = strtok(buffer, "\n");
+            while (line != NULL) {
+                parse_line(line);
+                line = strtok(NULL, "\n");
+            }
+        } else {
+            sleep(1);
+        }
     }
-}
+
     return 0;
 }
